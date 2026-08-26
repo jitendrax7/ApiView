8aec3770294d8e8d537c91011e36dd51c5254e34
jitendrax7 <rathourjitendrasingh472@gmail.com>
2026-08-26 23:58:12 +0530

    write a register & login & getprofile auth service

--- server/src/services/auth/service/authService.js
+++ server/src/services/auth/service/authService.js
@@ -2,6 +2,8 @@ import config from "../../../shared/config/index.js";
 import logger from "../../../shared/config/logger.js";
 import AppError from "../../../shared/utils/AppError.js";   
 import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs";
+
 
 export class AuthService {
     constructor(userRepository) {
@@ -34,6 +36,11 @@ export class AuthService {
         return useObj;
     }
 
+
+    async comparePassword(plainPassword, hashedPassword){
+        return bcrypt.compare(plainPassword, hashedPassword);
+    }
+
     async onboardSuperAdmin(superAdminData) {
         try {
             const existingUser = await this.userRepository.findAll();
@@ -55,6 +62,80 @@ export class AuthService {
         } catch (error) {
             logger.error("Error in onboarding Super admin", error);
             throw error
+        }
+    }
+
+    async register(userData) {
+        try {
+            const existingUser = await this.userRepository.findByUsername(userData.username);
+            if(existingUser){
+                throw new AppError('Username already exists', 409);
+            }
+
+            const existingEmail = await this.userRepository.findByEmail(userData.email);
+            if(existingEmail){
+                throw new AppError('Email already exists', 409);
+            }
+
+            const user = await this.userRepository.create(userData);
+            const token = this.generateToken(user);
+
+            logger.info("User registered successfully",{
+                username: user.username
+            });
+
+            return {
+                user:this.formateUserForResponce(user),
+                token
+            }
+
+        } catch (error) {
+            logger.error("Error in register service registering user", error);
+            throw error;
+        }
+    }
+
+    async login(username, password) {
+        try {
+            const user = await this.userRepository.findByUsername(username);
+            if(!user){
+                throw new AppError('Invalid credentials', 401);
+            }
+
+            if(!user.isActive){
+                throw new AppError('User account is deactivated', 403);
+            }
+
+            const isPasswordValid = await this.comparePassword(password, user.password);
+            if(!isPasswordValid){
+                throw new AppError('Invalid credentials', 401);
+            }
+            const token = this.generateToken(user);
+
+            logger.info("User logged in successfully",{
+                username: user.username
+            });
+            return {
+                user: this.formateUserForResponce(user),
+                token
+            };
+        } catch (error) {
+            logger.error("Error in login service", error);
+            throw error;
+        }
+    }
+
+
+    async getProfile(userId) {
+        try {
+            const user = await this.userRepository.findById(userId);
+            if(!user){
+                throw new AppError('User not found', 404);
+            }
+            return this.formateUserForResponce(user);
+        } catch (error) {
+            logger.error("Error in getProfile service", error);
+            throw error;
         }
     }
 }
