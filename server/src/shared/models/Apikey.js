cacd1ebb49c8296ca0c12a3bc0caa55dbab62387
jitendrax7 <rathourjitendrasingh472@gmail.com>
2026-08-29 23:58:40 +0530

    fix some error in apikey model

--- server/src/shared/models/Apikey.js
+++ server/src/shared/models/Apikey.js
@@ -95,7 +95,6 @@ const apiKeySchema = new mongoose.Schema(
                 const days = parseInt(process.env.API_KEY_EXPIRY_DAYS || '365');
                 return new Date(Date.now() + days * 24 * 60 * 60 * 1000);
             },
-            index: true,
         },
         metadata: {
             createdBy: {
