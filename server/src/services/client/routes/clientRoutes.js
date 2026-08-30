cf320b1eda1e6f664f14abf733e51710eef8791c
jitendrax7 <rathourjitendrasingh472@gmail.com>
2026-08-31 00:14:21 +0530

    create  a routes to create new client user and api key

--- server/src/services/client/routes/clientRoutes.js
+++ server/src/services/client/routes/clientRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import clientDependencies from "../dependencies/dependencies.js";
+import clientDependencies from "../Dependencies/dependencies.js";
 import authenticate from "../../../shared/middlewares/authenticate.js";
 
 const router = express.Router();
@@ -10,7 +10,10 @@ const {clientController} = clientDependencies.controllers;
 router.use(authenticate);
 
 router.post("/admin/clients/onboard",(req,res,next) => clientController.createClient(req, res, next));
+router.post("/admin/clients/:clientId/users",(req,res,next) => clientController.createClientUser(req, res, next));
+router.post("/admin/clients/:clientId/api/keys",(req,res,next) => clientController.createApiKey(req, res, next));
 
+router.get("/admin/clients/:clientId/api/keys",(req,res,next) => clientController.getClientApiKeys(req, res, next));
 
 
 export default router;
