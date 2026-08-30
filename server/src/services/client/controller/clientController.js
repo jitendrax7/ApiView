807ead398a6751a0104d2310f44a18762eb2bdbe
jitendrax7 <rathourjitendrasingh472@gmail.com>
2026-08-31 00:13:35 +0530

    write a new controller for create a new client user and api key

--- server/src/services/client/controller/clientController.js
+++ server/src/services/client/controller/clientController.js
@@ -29,4 +29,44 @@ export class ClientController {
             next(error);
         }
     }
+
+    /** Creates a new user for a specific client.
+     * @param {Object} req - The request object containing clientId and user data.
+     * @param {Object} res - The response object to send the result.
+     * @param {Function} next - The next middleware function for error handling.
+     * @returns {Promise<void>} - A promise that resolves when the user is created or an error occurs.
+     * @throws {Error} - Throws an error if the user creation fails.
+     */
+    async createClientUser(req, res, next) {
+        try {
+            const { clientId } = req.params;
+            const user = await this.clientService.createClientUser(clientId, req.body, req.user);
+            return res.status(201).json(ResponseFormatter.success(user, "Client user created successfully", 201));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    
+    async createApiKey(req, res, next) {
+        try {
+            const { clientId } = req.params;
+            const apikey = await this.clientService.createApiKey(clientId, req.body, req.user);
+            return res.status(201).json(ResponseFormatter.success(apikey, "API key created successfully", 201));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+
+    async getClientApiKeys(req, res, next) {
+        try {
+            const { clientId } = req.params;
+            const apiKeys = await this.clientService.getClientApiKeys(clientId, req.user);
+            return res.status(200).json(ResponseFormatter.success(apiKeys, "API keys retrieved successfully", 200));
+        } catch (error) {
+            next(error);
+        }
+    }
+
 }
